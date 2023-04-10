f3cf12c835594e584f0e3207d8ce4e1b20a918df
curlyfriesplease <jmulhern85@gmail.com>
2023-04-10 22:39:05 +0100

    Denies mobile view

--- src/App.js
+++ src/App.js
@@ -4,17 +4,49 @@ import { Image } from './components/image';
 import { useState } from 'react';
 import { generateArtistName } from './functions/generateArtistName';
 import { ArtCard } from './components/artCard';
+import { DenyMobileView } from './components/denyMobileView';
+import { useEffect } from 'react';
 
 function App() {
   const [randomImage, setRandomImage] = useState(null);
+  const [viewedFromMobile, setViewedFromMobile] = useState(false);
+  const [windowWidth, setWindowWidth] = useState([window.innerWidth]);
+
+  useEffect(() => {
+    if (window.innerWidth > 899) {
+      setViewedFromMobile(false);
+    } else if (window.innerWidth < 900) {
+      setViewedFromMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+      if (windowWidth < 900) {
+        setViewedFromMobile(true);
+      } else {
+        setViewedFromMobile(false);
+      }
+    };
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
 
   return (
-    <div className="App">
-      <div className="innerBody">
-        <Image randomImage={randomImage} setRandomImage={setRandomImage} />
-        <ArtCard />
-      </div>
-    </div>
+    <>
+      {viewedFromMobile && <DenyMobileView />}
+      {!viewedFromMobile && (
+        <div className="App">
+          <div className="innerBody">
+            <Image randomImage={randomImage} setRandomImage={setRandomImage} />
+            <ArtCard />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/denyMobileView.js
@@ -0,0 +1,32 @@
+import '../styles/denyMobileView.css';
+
+export const DenyMobileView = () => {
+  return (
+    <section>
+      <div class="scroll text1">
+        <div>
+          Desktop view only!<span> Not available on mobile! </span>Desktop view
+          only!
+          <span>Not available on mobile! </span>
+        </div>
+        <div>
+          Desktop view only!<span> Not available on mobile! </span>Desktop view
+          only!
+          <span>Not available on mobile! </span>
+        </div>
+      </div>
+      <div class="scroll text2">
+        <div>
+          It's not worth the effort making it compatible!
+          <span> It isn't that funny!</span> It's not worth the effort!
+          <span> It isn't that funny!</span>
+        </div>
+        <div>
+          It's not worth the effort making it compatible!
+          <span> It isn't that funny!</span> It's not worth the effort!
+          <span> It isn't that funny!</span>
+        </div>
+      </div>
+    </section>
+  );
+};
